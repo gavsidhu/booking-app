dd316beeedac08039dcc41de8db5c37481d00f9c
Gavin Sidhu <gavinjeet.sidhu@gmail.com>
2023-05-22 02:51:03 -0700

    added scheduling-platform.vercel.app as allowed host

--- booking_app/settings.py
+++ booking_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['scheduling-platform.onrender.com']
+ALLOWED_HOSTS = ['scheduling-platform.onrender.com','scheduling-platform.vercel.app']
 
 
 # Application definition
