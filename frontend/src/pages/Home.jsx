c7bf67ed2bd69a2c93c9863c3c609ea9b4369172
Gavin Sidhu <gavinjeet.sidhu@gmail.com>
2023-05-22 19:02:52 -0700

    commented out booking table from home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -77,7 +77,7 @@ export default function Home() {
               );
             })}
         </div>
-        <BookingsTable bookingTypes={bookingTypes} />
+        {/* <BookingsTable bookingTypes={bookingTypes} /> */}
       </div>
     </>
   );
